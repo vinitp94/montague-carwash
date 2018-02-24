959f520bcd43a7a39f7dc171e41f6dc075dcd3a0
Vinit Parikh <vinitp94@gmail.com>
2018-02-24 15:53:41 -0800

    Complete home page

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -9,7 +9,22 @@ export default function Home() {
       <Header activePage={'home'}></Header>
 
       <div className="main">
-        Home
+        <div className="welcome">Welcome to Montague Valero</div>
+        <div className="hand-car-wash">100% Hand Car Wash</div>
+
+        <div className="separator"></div>
+
+        <div className="message">
+          At Montague Valero 100% Hand Car Wash, we believe in quality, speed, customer service, and value.
+          We made a pledge to deliver quality worksmanship and unparalleled customer service while taking
+          excellent care of your vehicle. With trained employees and top-tier products, you should expect to
+          find your car clean in a short time.
+        </div>
+
+        <div className="punch-line">
+          We invite you to visit us and feel the difference that only a
+          hand car wash can deliver...
+        </div>
       </div>
 
       <Footer></Footer>
