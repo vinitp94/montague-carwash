82dcc43c95b4d8f9adc4b4e308572b4430a9b7d6
Vinit Parikh <vinitp94@gmail.com>
2018-03-03 15:14:40 -0800

    Develop contact page

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -9,7 +9,53 @@ export default function Contact() {
       <Header activePage={'contact'}></Header>
 
       <div className="main">
-        Contact
+        <div className="map-container">
+          <img src="src/assets/images/map-screenshot.png" alt="Map" />
+        </div>
+
+        <div className="info-container">
+          <div className="contact-us">
+            <div className="header">Contact Us</div>
+            <div className="content">(408) 955-0238</div>
+          </div>
+
+          <div className="location">
+            <div className="header">Location</div>
+            <div className="content">1298 Montague Exp Way, San Jose, CA - 95131</div>
+          </div>
+
+          <div className="hours">
+            <div className="header">Hours</div>
+
+            <div className="car-wash">
+              <div className="sub-title">Car Wash:</div>
+
+              <div className="content">
+                <div className="label">Winter:</div>
+                <div className="info">8:00 AM - 5:00 PM</div>
+              </div>
+
+              <div className="content">
+                <div className="label">Summer:</div>
+                <div className="info">8:00 AM - 7:00 PM -- (6:00 PM on Sundays)</div>
+              </div>
+            </div>
+
+            <div className="gas-station">
+              <div className="sub-title">Gas Station:</div>
+
+              <div className="content">
+                <div className="label">Mon-Fri:</div>
+                <div className="info">6:00 AM - 10:00 PM</div>
+              </div>
+
+              <div className="content">
+                <div className="label">Sat-Sun:</div>
+                <div className="info">7:00 AM - 9:00 PM</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       <Footer></Footer>
