b91ac166785daf018fa31726eef86d5ede69f77b
vinitp94 <vinitp94@gmail.com>
2018-05-01 15:17:48 -0700

    Add animation to header logo

--- src/components/building-blocks/header.js
+++ src/components/building-blocks/header.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 class Header extends React.Component {
   componentDidMount() {
     $('.welcome').addClass('selected');
-    
+
     $(document).scroll(() => {
       var scrollTop = $(document).scrollTop();
-      
+
       if (scrollTop < 410) {
         $('.welcome').addClass('selected');
         $('.deals').removeClass('selected');
@@ -21,32 +21,37 @@ class Header extends React.Component {
         $('.welcome').removeClass('selected');
         $('.deals').removeClass('selected');
       }
-      
-      if (scrollTop < 250) {
-        var percent = scrollTop / 250;
-        
-        $('.mv-header').height(140 - (70 * percent));
+
+      if (scrollTop < 300) {
+        var scalingFactor = scrollTop / 300;
+
+        $('.mv-header').height(140 - (70 * scalingFactor));
+        $('.header-logo').height(80 - (40 * scalingFactor));
       }
     });
   }
-  
+
   scrollToPosition(targetElement) {
-    var positionToScroll = $('.' + targetElementClass).offset().top - 100;
-    
+    var positionToScroll = 0;
+
+    if (targetElement === 'deals') {
+      positionToScroll = $('.mv-deals').offset().top - 50;
+    } else if (targetElement === 'contact') {
+      positionToScroll = $(document).height();
+    }
+
     window.scroll({
       top: positionToScroll,
       left: 0,
       behavior: 'smooth'
     });
   }
-  
+
   render() {
     return (
       <div className="mv-header">
         <div className="header-container">
-          <div className="header-logo">
-            <img src="src/assets/icons/valero-logo.png" alt="Logo" />
-          </div>
+          <img className="header-logo" src="src/assets/icons/valero-logo.png" alt="Logo" />
 
           <div className="nav">
             <div className="welcome" onClick={ this.scrollToPosition.bind(this, 'welcome') }>Home</div>
