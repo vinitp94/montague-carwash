c2940042362ef17e0fde2c7e2461d6dffa8f0e96
vinitp94 <vinitp94@gmail.com>
2018-05-01 14:33:04 -0700

    Add underline and scrolling using navbar

--- src/components/building-blocks/header.js
+++ src/components/building-blocks/header.js
@@ -1,25 +1,56 @@
 import React    from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Header(props) {
-
-  function applySelectedClass(page) {
-    if (page === props.activePage) {  return 'selected'; }
+class Header extends React.Component {
+  componentDidMount() {
+    $('.welcome').addClass('selected');
+    
+    $(document).scroll(() => {
+      var scrollTop = $(document).scrollTop();
+      
+      if (scrollTop < 420) {
+        $('.welcome').addClass('selected');
+        $('.deals').removeClass('selected');
+        $('.contact').removeClass('selected');
+      } else if (scrollTop < 1740) {
+        $('.deals').addClass('selected');
+        $('.welcome').removeClass('selected');
+        $('.contact').removeClass('selected');
+      } else {
+        $('.contact').addClass('selected');
+        $('.welcome').removeClass('selected');
+        $('.deals').removeClass('selected');
+      }
+    });
   }
+  
+  scrollToPosition(targetElementClass) {
+    var positionToScroll = $('.' + targetElementClass).offset().top - 140;
+    
+    window.scroll({
+      top: positionToScroll,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+  
+  render() {
+    return (
+      <div className="mv-header">
+        <div className="header-container">
+          <div className="header-logo">
+            <img src="src/assets/icons/valero-logo.png" alt="Logo" />
+          </div>
 
-  return (
-    <div className="mv-header">
-      <div className="header-container">
-        <Link className="header-logo" to={'/'}>
-          <img src="src/assets/icons/valero-logo.png" alt="Logo" />
-        </Link>
-
-        <div className="nav">
-          <Link className={ applySelectedClass('home') }    to={'/'}>Home</Link>
-          <Link className={ applySelectedClass('deals') }   to={'/deals'}>Deals</Link>
-          <Link className={ applySelectedClass('contact') } to={'/contact'}>Contact</Link>
+          <div className="nav">
+            <div className="welcome" onClick={ this.scrollToPosition.bind(this, 'mv-welcome') }>Home</div>
+            <div className="deals" onClick={ this.scrollToPosition.bind(this, 'mv-deals') }>Deals</div>
+            <div className="contact" onClick={ this.scrollToPosition.bind(this, 'mv-contact') }>Contact</div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Header;
