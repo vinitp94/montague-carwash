c267110bc6db9ff6bbbde8c9e7b2093c7cefd695
Vinit Parikh <vinitp94@gmail.com>
2018-05-20 17:14:15 -0700

    Fix up 404 page

--- src/components/pages/page-not-found.js
+++ src/components/pages/page-not-found.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function PageNotFound() {
   return (
     <div className="mv-page-not-found">
-      Page not found
+      Page not found.
     </div>
   );
 }
