094c2f7a5cee02275e2e1f6ec4aea2ed05e476a5
Vinit Parikh <vinitp94@gmail.com>
2019-01-29 21:28:34 -0800

    Update prices

--- src/components/building-blocks/deals.js
+++ src/components/building-blocks/deals.js
@@ -12,16 +12,17 @@ export default function Deals() {
           <div className="feature">Hand wash</div>
           <div className="feature">Hand dry</div>
           <div className="feature">Window cleaning</div>
-          <div className="price">Price: $18.95</div>
+          <div className="feature">Tire dressing</div>
+          <div className="price">Price: $19.95</div>
         </div>
 
         <div className="standard-unit">
-          <div className="name">Premier Basic</div>
+          <div className="name">Premier Hand Wash</div>
           <div className="feature">Hand wash</div>
           <div className="feature">Hand dry</div>
           <div className="feature">Inside & outside glass cleaning</div>
           <div className="feature">Interior vacuum</div>
-          <div className="price">Price: $24.95</div>
+          <div className="price">Price: $29.95</div>
         </div>
 
         <div className="standard-unit">
@@ -35,7 +36,7 @@ export default function Deals() {
           <div className="feature">Tire dressing</div>
           <div className="feature">Fragrance</div>
           <div className="feature">Water spot & tree sap removal (extra charges)</div>
-          <div className="price">Price: $29.95</div>
+          <div className="price">Price: $34.95</div>
         </div>
 
         <div className="standard-unit">
@@ -43,14 +44,15 @@ export default function Deals() {
           <div className="feature">** Includes all VIP Hand Wash features</div>
           <div className="feature">Front floor mat wash and clean</div>
           <div className="feature">Dashboard & door creme</div>
-          <div className="price">Price: $35.95</div>
+          <div className="price">Price: $44.95</div>
         </div>
 
         <div className="standard-unit">
-          <div className="name">Express Spray Wax</div>
-          <div className="feature">** Includes all VIP Hand Wash features</div>
-          <div className="feature">Spray wax</div>
-          <div className="price">Price: $39.95</div>
+          <div className="name">Exterior Hand Wash & Hand Wax</div>
+          <div className="feature">** Includes all Exterior Hand Wash features</div>
+          <div className="feature">Wax (applied by hand)</div>
+          <div className="feature">Carnuba wax (add $10)</div>
+          <div className="price">Price: $49.95</div>
         </div>
 
         <div className="standard-unit">
@@ -58,7 +60,7 @@ export default function Deals() {
           <div className="feature">** Includes all VIP Hand Wash features</div>
           <div className="feature">Wax (applied by hand)</div>
           <div className="feature">Carnuba wax (add $10)</div>
-          <div className="price">Price: $49.95</div>
+          <div className="price">Price: $59.95</div>
         </div>
       </div>
 
@@ -70,7 +72,7 @@ export default function Deals() {
           <div className="feature">** Includes all VIP Hand Wash features</div>
           <div className="feature">Carnuba wax (applied by hand)</div>
           <div className="feature">Clay bar paint surface (applied by hand)</div>
-          <div className="price">Price: $79.95</div>
+          <div className="price">Price: $99.95</div>
         </div>
 
         <div className="mini-detail-unit">
@@ -79,7 +81,7 @@ export default function Deals() {
           <div className="feature">Dashboard, door, & console creme</div>
           <div className="feature">Carnuba wax (applied by hand)</div>
           <div className="feature">Seat OR carpet shampoo (excluding leather)</div>
-          <div className="price">Price: $89.95</div>
+          <div className="price">Price: $119.95</div>
         </div>
 
         <div className="mini-detail-unit">
@@ -94,14 +96,14 @@ export default function Deals() {
           <div className="feature">Lift solids & remove most spots/stains</div>
           <div className="feature">Carpets & mats cleaning</div>
           <div className="feature">Fragrance</div>
-          <div className="price">Price: $45.95</div>
+          <div className="price">Price: $59.95</div>
         </div>
 
         <div className="mini-detail-unit">
           <div className="name">Seat shampoo (non-leather)</div>
           <div className="feature">Lift solids & remove most spots/stains</div>
           <div className="feature">Fragrance</div>
-          <div className="price">Price: $49.95</div>
+          <div className="price">Price: $59.95</div>
         </div>
 
         <div className="mini-detail-unit">
@@ -126,7 +128,7 @@ export default function Deals() {
           <div className="feature">Carpet & seat shampoo</div>
           <div className="feature">Floor mat cleaning</div>
           <div className="feature">Fragrance</div>
-          <div className="price">Price: $109.95</div>
+          <div className="price">Price: $149.95</div>
         </div>
 
         <div className="full-detail-unit">
@@ -139,14 +141,14 @@ export default function Deals() {
           <div className="feature">Water spot & tree sap removal</div>
           <div className="feature">Carnuba wax (applied by hand)</div>
           <div className="feature">Clay bar paint surface (applied by hand)</div>
-          <div className="price">Price: $129.95</div>
+          <div className="price">Price: $119.95</div>
         </div>
 
         <div className="full-detail-unit">
           <div className="name">Interior & Exterior Combination</div>
           <div className="feature">** Includes all Interior Detail features</div>
           <div className="feature">** Includes all Exterior Detail features</div>
-          <div className="price">Price: $199.95</div>
+          <div className="price">Price: $229.95</div>
         </div>
       </div>
     </div>
