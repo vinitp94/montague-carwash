87ac167123cbad323c3bb05150493508d3c75e38
Vinit Parikh <vinitp94@gmail.com>
2022-11-27 11:12:47 -0800

    update footer

--- src/components/building-blocks/footer.js
+++ src/components/building-blocks/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className='mv-footer'>
-      <div className='label'>Developed by <a className='developer' href='https://www.mach4.app/' target='_blank'>MACH4 Technologies Inc.</a></div>
+      <div className='label'>Developed by MACH4 Technologies Inc.</div>
     </div>
   );
 }
